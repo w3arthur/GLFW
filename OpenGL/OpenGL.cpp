fb758d03df870d75f0a463501055e09b703dfed3
Arthur Zarankin <78190695+w3arthur@users.noreply.github.com>
2023-01-01 09:25:15 +0200

    Vertex Attributes

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -1,8 +1,6 @@
-// OpenGL.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// docs https://docs.gl/
 #include <iostream>
-#define GLEW_STATIC     //Can set on C++ preprocessor //GLOW_BUILD
+#define GLEW_STATIC     //Can set on C++ preprocessor, for Dll set GLOW_BUILD
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -21,18 +19,44 @@ int main()
     if (glewInit() != GLEW_OK) { cout << "GLEW Error!" << endl; return EXIT_FAILURE_GLEW; } //GLEW will set only here
     cout << "Gl version: " << glGetString(GL_VERSION) << endl;
 
+    float positions[] = 
+    {
+        -0.5f, -0.5f
+        , 0.0f, 0.5f
+        , 0.5f, -0.5f
+    };   //6 floats, our buffer, each line vertex (point)
+    int vertexAttributeSize = 2;
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);   //select buffer to use
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);    //buffer of memory, using the one we generated
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(0, vertexAttributeSize, GL_FLOAT, GL_FALSE, sizeof(float) * vertexAttributeSize,  0); // 0 start 2 jumppby, type, normalize?, stride (distance between each vertex), pointer (number of attribute) sizeof(float) * (place -1)
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
     while ( !glfwWindowShouldClose(window) )
     {
         glClear(GL_COLOR_BUFFER_BIT);   // add opengl32.lib
         glClearColor(0.0f, 0.5f, 1.0f, 0.0f);
-        glBegin(GL_TRIANGLES);// -1 ... 1
-        glVertex2f(0.0f, 0.75f);
-        glVertex2f(-0.25f, -0.5f); glVertex2f(0.25f, -0.5f);
-        glEnd();
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.25f, 0.5f); glVertex2f(0.25f, 0.5f);
-        glVertex2f(0.0f, -0.75f);
-        glEnd();
+        //glBegin(GL_TRIANGLES);// -1 ... 1
+        //glVertex2f(0.0f, 0.75f);
+        //glVertex2f(-0.25f, -0.5f); glVertex2f(0.25f, -0.5f);
+        //glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3); // 0begin - objects    // will draw our buffer, bined to position
+        //glDrawElements(GL_TRIANGLES, 3, );
+
+
+
+        //glBegin(GL_TRIANGLES);
+        //glVertex2f(-0.25f, 0.5f); 
+        //glVertex2f(0.25f, 0.5f);
+        //glVertex2f(0.0f, -0.75f);
+        //glEnd();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
